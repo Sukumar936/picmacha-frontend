37f00697b9f13c009fc0310307c099fd42d5e365
Sukumar936 <msukumar962k@email.com>
2025-08-18 02:10:10 +0530

    1 st commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to PicMacha 📸</h1>
+      <h1>Welcome to PicMacha 📸 Testin 1</h1>
       <p>Manage your photo galleries with ease</p>
       <Link to="/login">Login</Link> | <Link to="/signup">Sign Up</Link>
     </div>
